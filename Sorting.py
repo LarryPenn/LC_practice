9ddc804254988ea7af9671f33bbc3efc0d185321
LarryPenn <49259555+LarryPenn@users.noreply.github.com>
2020-09-02 10:18:59 -0700

    topKFrequent

--- Sorting.py
+++ Sorting.py
@@ -25,4 +25,31 @@ class Solution:
                 i += 1
         
         
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/799/
+
+    
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        d = {}
+        result = []
+        
+        for num in nums:
+            if num in d:
+                d[num] += 1
+            else:
+                d[num] = 1
+                
+        for i in range (0, k):
+            max = 0
+            res = None
+            for key in d.keys():
+                if d[key] > max:
+                    res = key
+                    max = d[key]
+        
+            result.append(res)
+            d.pop(res)
+            
+        return result
+                
